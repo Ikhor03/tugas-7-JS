765265d54216e1f6cccca2fdcc92cb285c616039
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-13 13:49:32 +0700

    task7

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+let tinggiArr = [154, 156, 175, 160, 167, 180, 165, 150, 170, 172, 152, 168];
+
+for (let i = 0; i < tinggiArr.length; i++) {
+    console.log(tinggiArr[i]);
+}
+
+for (let i of tinggiArr) {
+    console.log(i);
+}
+
+// Selain di syntaxnya, tidak ada perbedaan fungsi yang mencolok antara for dan for of pada kasus ini
